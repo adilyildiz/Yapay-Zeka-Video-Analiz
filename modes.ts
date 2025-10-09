8725d385aa8b3101fe4d1eb54ab6d589b60d2f51
Adil YILDIZ <adilyildiz@gmail.com>
2025-10-09 17:59:41 +0300

    Mini Düzeltme

--- modes.ts
+++ modes.ts
@@ -185,7 +185,7 @@ const modes: Record<string, Mode> = {
 Tüm analiz sonuçları Türkçe olmalıdır.`,
     isList: true,
     subModes: {
-      'Özel Kategoriler': '',
+      'Özel Kategoriler': 'fun, challenge, behavioural momentum, rewards, penalties, pavlovian interaction, urgent optimism, communal discovery, strategy/planning, story, cooperation, pareto optimal, feedback, protege effect, mini games, design/editing, realism, ownership, role play, virality, cascading information, collaboration, competition, cut scenes, action points, levels, tokens, question&answer, game turns, selecting/collecting, resource management, capture/eliminate, feedback, goods/information, time pressure, tutorial, tiles/grids, infinite gameplay, appointment, movement, assessment, status, simulate, response',
     },
   },
   'Görsel-İşitsel Altyazılar': {
